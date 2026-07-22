349f74e61cec68fc325196a8a73a8819137c2f9b
risreb <k.salamakha04@mail.ru>
2026-07-22 22:20:22 +0300

    lab6.1
    Сумматор

---
+++ lab6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+
+using namespace std;
+
+int main()
+
+{
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int our_num = 0;
+    int user_int;
+
+    do
+    {
+        cout << "Введите целое число или число '0', чтобы закончить: " << endl;
+        cin >> user_int;
+        our_num += user_int;
+    } 
+    while (user_int != 0);
+        {
+        cout << "Сумма: " << our_num << endl;
+        }
+  
+    
+
+    return 0;
+}
