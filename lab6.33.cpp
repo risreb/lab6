3efa403c1dfe08d89fb964c04dc269957ce48d82
risreb <k.salamakha04@mail.ru>
2026-07-23 09:08:21 +0300

    lab6.3
    Таблица умножения для числа

---
+++ lab6.33.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    int number;
+    cout << "Введите целое число: " << endl;
+    cin >> number;
+
+        for (int i = 1; i <= 10; i++)
+
+        {
+
+            cout << number << " х " << i << " = " << number * i << endl;
+
+        }
+
+        return 0;
+
+    }
